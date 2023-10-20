d147c2302e3cb8cec60a9e0a8ac0ca0d6f35f246
Abdisamad Nuh <abdisamad.nuh@outlook.com>
2023-10-20 17:13:54 +0200

    Searchpage 1.o v

--- app/src/main/java/com/example/pokedex/MainActivity.kt
+++ app/src/main/java/com/example/pokedex/MainActivity.kt
@@ -1,17 +1,46 @@
-package com.example.pokedex
+package com.example.myexperiment
 
-import android.graphics.Color
+
+import android.media.Image
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.material3.Button
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.BasicText
+import androidx.compose.foundation.text.BasicTextField
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material.icons.filled.Clear
+import androidx.compose.material.icons.filled.Face
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.material.icons.rounded.Face
+import androidx.compose.material3.Divider
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -19,71 +48,114 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import com.example.pokedex.ui.theme.PokedexTheme
+import androidx.compose.ui.unit.sp
+import org.intellij.lang.annotations.JdkConstants.HorizontalAlignment
 
+@OptIn(ExperimentalMaterial3Api::class)
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            PokedexTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("GUYS")
-                    DemoScreen()
-                }
+
+            SearchPage()
+
+        }
+    }
+}
+
+
+@Preview(showBackground = true)
+@Composable
+fun SearchPage() {
+    var name by remember { mutableStateOf("Search your Pokemon") }
+    var Pokemons: MutableList<String> = mutableListOf()
+
+    Pokemons.add(0, "PokemonNames")
+
+    Column(
+        modifier = Modifier.fillMaxSize()
+    ) {
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .background(Color.LightGray, RoundedCornerShape(15.dp))
+                .height(38.dp),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Icon(imageVector = Icons.Default.ArrowBack, contentDescription = "backArrow")
+
+            BasicTextField(
+                value = name,
+                onValueChange = { text -> name = text },
+                modifier = Modifier.weight(1f)
+            )
+
+            if (name.isNotBlank()) {
+                Icon(
+                    imageVector = Icons.Default.Clear,
+                    contentDescription = "clear",
+                    modifier = Modifier.clickable {
+                        name = ""
+                    }
+                )
             }
         }
-    }
-}
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-@Composable
-fun DemoScreen() {
-    var isOn by remember {
-        mutableStateOf(false)
-    }
 
-    Box(Modifier.fillMaxSize(), Alignment.TopCenter) {
-        //Box(modifier = Modifier.size(40.dp), Alignment.TopCenter) {
-        SearchButton(isOn) {
-            isOn = !isOn
-        }
-    }
-        Box(Modifier.fillMaxSize(), Alignment.TopEnd) {
-        FilterButton(isOn) {
-            isOn = !isOn
-        }
-    }
-}
-@Composable
-fun SearchButton(isOn: Boolean, onClick: () -> Unit) {
-
-    Button(onClick = onClick) {
-        if (isOn) {
-            Text(text = "Off")
-        } else {
-            Text("🔍")
-        }
+        Pokemonlists(Pokemons = Pokemons)
     }
 }
 
 @Composable
-fun FilterButton(isOn: Boolean, onClick: () -> Unit) {
+fun Pokemonlists(Pokemons:List<String>
+                 ,modifier: Modifier =Modifier){
+    LazyColumn {
+        items (Pokemons){ currentPokemon ->
+            Row() {
+                Icon(imageVector = Icons.Default.Face , contentDescription ="Pokemon pictures" )
 
-    Button(onClick = onClick) {
-        if (isOn) {
-            Text(text = "Off")
-        } else {
-            Text("On")
+                Text(
+                    text = currentPokemon)
+                Divider()
+            }
+
+        }
+
+    }
+
+}
+
+@Composable
+fun filterPage(){
+    Column ( modifier = Modifier
+        .fillMaxSize()
+    ) {
+
+
+
+
+        Row (modifier = Modifier
+            .fillMaxWidth()
+            .height(38.dp),
+            verticalAlignment = Alignment.CenterVertically){
+
+            Icon(imageVector = Icons.Default.ArrowBack, contentDescription = "backArrow")
+
+            Text(text = "Filter Options",
+                fontSize =20.sp)
+
+
+
+
         }
     }
+
+
+
 }
+
+
