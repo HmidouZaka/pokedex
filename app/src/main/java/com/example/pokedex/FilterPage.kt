763e23daeffcf4a7a86cfe1c0197b25476bf037a
s224292 <s224292@student.dtu.dk>
2023-10-24 17:04:33 +0200

    Update FilterPage with Generation buttons and names under each button clicked. Maybe a dataobject would be better but for now it works like inteded.

--- app/src/main/java/com/example/pokedex/FilterPage.kt
+++ app/src/main/java/com/example/pokedex/FilterPage.kt
@@ -3,54 +3,97 @@ package com.example.pokedex
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
-import androidx.compose.material3.Icon
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 
-class FilterPage : ComponentActivity(){
+class FilterPage : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         setContent {
-            filterPage()
+            FilterPageContent()
         }
     }
-    @Composable
-    fun filterPage() {
-        Column(
-            modifier = Modifier
-                .fillMaxSize()
+}
+
+@Composable
+fun FilterPageContent() {
+    var selectedGeneration by remember { mutableStateOf(1) }
+
+    Column(
+        modifier = Modifier.fillMaxSize()
+    ) {
+        Row(
+            modifier = Modifier.fillMaxWidth().height(38.dp),
+            verticalAlignment = Alignment.CenterVertically
         ) {
+            Icon(imageVector = Icons.Default.ArrowBack, contentDescription = "backArrow")
+            Text(
+                text = "Filter Options",
+                fontSize = 20.sp
+            )
+        }
 
+        Spacer(modifier = Modifier.height(16.dp))
+//Vi laver lige 9 knapper via et for loop for generation, det er derfor names ik er med.
+        for (generation in 1..9) {
+            GenerationButton(
+                generation = generation,
+                selectedGeneration = selectedGeneration,
+                onGenerationSelected = { selectedGeneration = it }
+            )
 
-            Row(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .height(38.dp),
-                verticalAlignment = Alignment.CenterVertically
-            ) {
-
-                Icon(imageVector = Icons.Default.ArrowBack, contentDescription = "backArrow")
-
-                Text(
-                    text = "Filter Options",
-                    fontSize = 20.sp
-                )
-
+            if (selectedGeneration == generation) {
+                GenerationNameList(generation = generation)
             }
         }
 
-
+        // Type Button tilføjes her senere.
     }
 }
+
+@Composable
+fun GenerationButton(
+    generation: Int,
+    selectedGeneration: Int,
+    onGenerationSelected: (Int) -> Unit
+) {
+    Button(
+        onClick = { onGenerationSelected(generation) },
+        modifier = Modifier.padding(16.dp)
+    ) {
+        Text(text = "Generation $generation")
+    }
+}
+
+@Composable
+fun GenerationNameList(generation: Int) {
+    val namesForGeneration = when (generation) {
+        1 -> listOf("Red", "Green", "Blue", "Yellow")
+        2 -> listOf("Gold", "Silver", "Crystal")
+        3 -> listOf("Ruby", "Sapphire", "Emerald", "FireRed", "LeafGreen")
+        4 -> listOf("Diamond", "Pearl", "Platinum", "HeartGold", "SoulSilver")
+        5 -> listOf("Black", "White", "Black 2", "White 2")
+        6 -> listOf("X", "Y", "Omega Ruby", "Alpha Sapphire")
+        7 -> listOf("Sun", "Moon", "Ultra Sun", "Ultra Moon")
+        8 -> listOf("Sword", "Shield", "Brilliant Diamond", "Shining Pearl", "Legends: Arceus")
+        9 -> listOf("Scarlet", "Violet")
+        else -> emptyList()
+    }
+
+    Column(
+        modifier = Modifier.padding(16.dp),
+    ) {
+        namesForGeneration.forEach { name ->
+            Text(text = name, fontSize = 16.sp)
+        }
+    }
+}
+
