7346d58508b00180dfc1d3811e6a4ac97106bf85
zainabjh01 <91522375+zainabjh01@users.noreply.github.com>
2023-11-23 13:35:29 +0100

    Added filterviewmodel but maybe its gonna be deleted later

--- app/src/main/java/com/example/pokedex/FilterPage.kt
+++ app/src/main/java/com/example/pokedex/FilterPage.kt
@@ -26,7 +26,7 @@ import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import com.example.pokedex.FilterViewModel
+//import com.example.pokedex.FilterViewModel
 
 
 class FilterPage : ComponentActivity() {
---
+++ app/src/main/java/com/example/pokedex/FilterViewModel.kt
@@ -0,0 +1,15 @@
+package com.example.pokedex
+
+import androidx.compose.runtime.State
+import androidx.compose.runtime.mutableStateOf
+import androidx.lifecycle.ViewModel
+
+// ViewModel
+class FilterViewModel : ViewModel() {
+    private val _selectedSortOption = mutableStateOf<SortOption?>(null)
+    val selectedSortOption: State<SortOption?> = _selectedSortOption
+
+    fun setSortOption(sortOption: SortOption?) {
+        _selectedSortOption.value = sortOption
+    }
+}
