8da489f52c6ea429bc67526dd4735aaa20626c72
Abdisamad Nuh <abdisamad.nuh@outlook.com>
2023-11-30 14:55:59 +0100

    cucumber tests

--- app/src/androidTest/java/com/example/filter.feature
+++
@@ -1,4 +0,0 @@
-Feature:  Filter Functionality
-  Scenario: click filterbutton and see a dropdown
-    When I click  on a filterbutton
-    Then Then is should see a list of sub-categories.
--- app/src/androidTest/java/com/example/steps/stepDefinitions.kt
+++ app/src/androidTest/java/com/example/steps/stepDefinitions.kt
@@ -28,11 +28,14 @@ class FavouriteSteps {
     @When("I press the favourite logo on a Pokemon")
     fun i_press_the_favourite_logo_on_a_pokemon() {
         // Assuming you have a selected Pokemon, toggle its favorite status
-
+        model.PokemonsFave.clear()
         if(model.PokemonsFave.isEmpty())
             println("favourite is empty")
+        else
+            throw PendingException()
 
-        // model.toggleFavourite(model.Pokemons.get(0))
+
+         model.toggleFavourite(model.Pokemons.get(0))
 
 
 
@@ -42,30 +45,24 @@ class FavouriteSteps {
     @Then("the pokemon selected should be added to the Favouritelist.")
     fun the_pokemon_selected_should_be_on_the_favouritelist() {
 
-
+        if(model.PokemonsFave.isNotEmpty())
          println("this pokemon added to faveouritlist ${model.PokemonsFave.get(0)}")
-
+        else
+            throw PendingException()
     }
 
 
-    @When("I press the favourite logo on a Pokemon once")
-    fun i_press_the_favourite_logo_on_a_pokemon_once() {
-        // Assuming you have a selected Pokemon, toggle its favorite status
-       // model.toggleFavourite(model.Pokemons.get(0))
-        if(model.PokemonsFave.isEmpty())
-            println("favourite is empty")
-
-       // model.toggleFavourite(model.Pokemons.get(0))
-
-
-
-
-    }
 
     @Then("the pokemon selected should be removed from  the Favouritelist.")
     fun the_pokemon_selected_should_be_removed_from_the_favouritelist() {
         // Write code here that turns the phrase above into concrete actions
         //throw PendingException()
+        model.toggleFavourite(model.Pokemons.get(0))
+
+        if(model.PokemonsFave.isEmpty())
+            println("the pokemon was removed")
+        else
+            throw PendingException()
     }
 
 
@@ -82,19 +79,6 @@ class FavouriteSteps {
     }
 
 
-    @When("I start the program")
-    fun i_start_the_program() {
-       // ApiViewModel().addPokemon(1,40,true,true)
-
-        // Write code here that turns the phrase above into concrete actions
-        throw PendingException()
-    }
-
-    @Then("Then pokemons should be loaded from API")
-    fun then_pokemons_should_be_loaded_from_api() {
-        // Write code here that turns the phrase above into concrete actions
-        throw PendingException()
-    }
 
 
 }
